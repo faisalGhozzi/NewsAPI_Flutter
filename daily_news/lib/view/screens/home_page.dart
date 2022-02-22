4eaf9a20122158ae5679213c897c3310ac0656cc
unknown <faicel.ghozzi@esprit.tn>
2022-02-22 23:22:59 +0100

    body separation

--- daily_news/lib/view/screens/home_page.dart
+++ daily_news/lib/view/screens/home_page.dart
@@ -1,75 +1,29 @@
-import 'package:daily_news/model/article_model.dart';
-import 'package:daily_news/view/screens/show_details.dart';
-import 'package:daily_news/view/widgets/drop_shadow_widget.dart';
-import 'package:daily_news/viewmodel/article_view_model.dart';
+import 'package:daily_news/view/screens/news_posts.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+
+// class HomePage extends StatelessWidget {
+//   const HomePage({ Key? key }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: NewsPosts(),
+//     );
+//   }
+// }
+
 class HomePage extends StatelessWidget {
   const HomePage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    ArticleViewModel articleViewModel = context.watch<ArticleViewModel>();
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(),
-      body: articleViewModel.loading ? const Center(child: CircularProgressIndicator()) : Container(
-        child: Column(children: [
-          DropShadowWidget(child: Column(
-            children: const [
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text("NNews",
-                style: TextStyle(
-                  fontFamily: 'PlayfairDisplay',
-                  fontSize: 40,
-                ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Text("your daily dose of news",
-                style: TextStyle(
-                  fontFamily: 'Nunito',
-                  fontSize: 20,
-                ),
-                ),
-              ),
-            ],
-          ), 
-          height: .1,
-          opacity: 0.2,),
-          _ui(articleViewModel)]),
-      ),
+      body: NewsPosts(),
     );
   }
 
-  _ui(ArticleViewModel articleViewModel){
-    return Expanded(
-      child: ListView.separated(
-        itemBuilder: ((context, index) {
-          Article article = articleViewModel.articleList[index];
-          return Container(
-            padding: const EdgeInsets.all(2),
-            child: ListTile(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (_) => ShowDetails(article: article)
-                ));
-              },
-              title: Text(article.title, maxLines: 1, overflow: TextOverflow.ellipsis,),
-              subtitle: Text("Source : "+article.source.name),
-              leading: Container(
-                width: MediaQuery.of(context).size.width* .3,
-                decoration: BoxDecoration(
-                  image: DecorationImage(image: NetworkImage(article.urlToImage)),
-                ),
-              ),
-              ),
-          );
-        }), 
-        separatorBuilder: (context, index) => const Divider(), 
-        itemCount: articleViewModel.articleList.length),
-        );
-  }
+  
 }
---
+++ daily_news/lib/view/screens/news_posts.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:provider/provider.dart';
+import 'package:daily_news/model/article_model.dart';
+import 'package:daily_news/view/screens/show_details.dart';
+import 'package:daily_news/view/widgets/drop_shadow_widget.dart';
+import 'package:daily_news/viewmodel/article_view_model.dart';
+
+class NewsPosts extends StatefulWidget {
+  const NewsPosts({ Key? key }) : super(key: key);
+
+  @override
+  _NewsPostsState createState() => _NewsPostsState();
+}
+
+class _NewsPostsState extends State<NewsPosts> {
+  @override
+  Widget build(BuildContext context) {
+    ArticleViewModel articleViewModel = context.watch<ArticleViewModel>();
+    return  articleViewModel.loading ? const Center(child: CircularProgressIndicator()) : Container(
+        child: Column(children: [
+          DropShadowWidget(child: Column(
+            children: const [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text("NNews",
+                style: TextStyle(
+                  fontFamily: 'PlayfairDisplay',
+                  fontSize: 40,
+                ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text("your daily dose of news",
+                style: TextStyle(
+                  fontFamily: 'Nunito',
+                  fontSize: 20,
+                ),
+                ),
+              ),
+            ],
+          ), 
+          height: .1,
+          opacity: 0.2,),
+          _ui(articleViewModel)]),
+      );
+  }
+
+  _ui(ArticleViewModel articleViewModel){
+    return Expanded(
+      child: ListView.separated(
+        itemBuilder: ((context, index) {
+          Article article = articleViewModel.articleList[index];
+          return Container(
+            padding: const EdgeInsets.all(2),
+            child: ListTile(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (_) => ShowDetails(article: article)
+                ));
+              },
+              title: Text(article.title, maxLines: 1, overflow: TextOverflow.ellipsis,),
+              subtitle: Text("Source : "+article.source.name),
+              leading: Container(
+                width: MediaQuery.of(context).size.width* .3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(image: NetworkImage(article.urlToImage)),
+                ),
+              ),
+              ),
+          );
+        }), 
+        separatorBuilder: (context, index) => const Divider(), 
+        itemCount: articleViewModel.articleList.length),
+        );
+  }
+}
